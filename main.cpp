0d019efc5cd396bb0bb822725aed003c455c8c68
Grimrukh <grimrukh@gmail.com>
2025-01-21 17:14:33 +1000

    Added GHA build (MSVC Release)

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <format>
+#include <string>
+
+#include "GrimHook/Logging.h"
+#include "GrimHookDSR/DSRHook.h"
+
+
+int main(int argc, char* argv[])
+{
+    const std::wstring msg = std::format(L"Logging Test. DSR Process Name: {}", GrimHookDSR::DSR_PROCESS_NAME);
+    GrimHook::Info(msg);
+    return 0;
+}
