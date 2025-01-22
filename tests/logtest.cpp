ed5d535555116d5a4e31b0649be02dba01708465
Grimrukh <grimrukh@gmail.com>
2025-01-22 10:34:04 +1000

    Set up CMake tests

---
+++ tests/logtest.cpp
@@ -0,0 +1,15 @@
+﻿#include <format>
+#include <string>
+
+#include "GrimHook/Logging.h"
+#include "GrimHookDSR/DSRHook.h"
+
+
+/// @brief Tests C++20 (format), GrimHookCore logging, and GrimHookDSR process name.
+int main()
+{
+    GrimHook::Info("GrimHook logging success.");
+    const std::wstring dsrMsg = std::format(L"DSR Process Name: '{}'", GrimHookDSR::DSR_PROCESS_NAME);
+    GrimHook::Info(dsrMsg);
+    return 0;
+}
